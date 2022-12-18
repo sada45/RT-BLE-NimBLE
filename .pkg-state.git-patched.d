b9d09dbda9627be676bc2a8efe993a29aa6a14b3
sada45 <sada45@ubuntu.ubuntu-domain>
2022-12-18 14:10:29 +0800

    RT-BLE_NimBLE

---
+++ .pkg-state.git-patched.d
@@ -0,0 +1,6 @@
+/mnt/hgfs/DATA_SHARE/RIOT_2022/build/pkg/nimble/.pkg-state.git-patched:  /mnt/hgfs/DATA_SHARE/RIOT_2022/build/pkg/nimble/.pkg-state.git-downloaded  Makefile /mnt/hgfs/DATA_SHARE/RIOT_2022/pkg/pkg.mk /mnt/hgfs/DATA_SHARE/RIOT_2022/makefiles/utils/variables.mk /mnt/hgfs/DATA_SHARE/RIOT_2022/build/pkg/nimble/.pkg-state.git-patched.d
+/mnt/hgfs/DATA_SHARE/RIOT_2022/build/pkg/nimble/.pkg-state.git-downloaded:
+Makefile:
+/mnt/hgfs/DATA_SHARE/RIOT_2022/pkg/pkg.mk:
+/mnt/hgfs/DATA_SHARE/RIOT_2022/makefiles/utils/variables.mk:
+/mnt/hgfs/DATA_SHARE/RIOT_2022/build/pkg/nimble/.pkg-state.git-patched.d:
